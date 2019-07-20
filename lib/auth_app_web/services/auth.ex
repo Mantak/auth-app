26285f1a9e6a5a2664a112239f9b7b99d5ca01e7
Ryan Angelo Cadag <ryan.cadag@secuna.io>
2019-07-20 20:01:01 +0800

    module to authenticate user

---
+++ lib/auth_app_web/services/auth.ex
@@ -0,0 +1,21 @@
+defmodule AuthAppWeb.Services.Auth do
+  alias Comeonin.Bcrypt
+  alias AuthApp.Accounts
+
+  def verify_account(email, password) do
+    case Accounts.get_user(:email, email) do
+      nil ->
+        {:error, :invalid_credentials}
+      user ->
+        validate_password(user, password)
+    end
+  end
+
+  def validate_password(%{password: password} = user, password_input) do
+    if Bcrypt.checkpw(password_input, password) do
+      {:ok, user}
+    else
+      {:error, :invalid_credentials}
+    end
+  end
+end
