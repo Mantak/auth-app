2f9a8b92cb2f08cec976ee4acf8a9189bdceac2c
Ryan Angelo Cadag <ryan.cadag@secuna.io>
2019-07-20 20:00:51 +0800

    schema for users table

---
+++ lib/auth_app/accounts/user.ex
@@ -0,0 +1,31 @@
+defmodule AuthApp.Accounts.User do
+  use Ecto.Schema
+  import Ecto.Changeset
+  alias Comeonin.Bcrypt
+
+  @primary_key {:id, :binary_id, autogenerate: true}
+  @email_format ~r/^[A-Za-z0-9._%+-+']+@[A-Za-z0-9.-]+\.[A-Za-z]+$/
+
+  schema "users" do
+    field :email, :string
+    field :password, :string
+
+    timestamps()
+  end
+
+  def changeset(user, attrs) do
+    user
+    |> cast(attrs, [:email, :password])
+    |> validate_required([:email, :password])
+    |> validate_length(:password, min: 6)
+    |> validate_format(:email, @email_format)
+    |> unique_constraint(:email)
+    |> hash_password()
+  end
+
+  def hash_password(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Bcrypt.hashpwsalt(password))
+  end
+
+  def hash_password(changeset), do: changeset
+end
