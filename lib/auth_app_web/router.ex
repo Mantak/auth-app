cc794edfbe096aabc25de652629aecf1d6549b61
Ryan Angelo Cadag <ryan.cadag@secuna.io>
2019-07-20 22:59:38 +0800

    guardian pipelines

--- lib/auth_app_web/router.ex
+++ lib/auth_app_web/router.ex
@@ -9,14 +9,30 @@ defmodule AuthAppWeb.Router do
     plug :put_secure_browser_headers
   end
 
+  pipeline :auth do
+    plug AuthAppWeb.Services.GuardianPipeline
+  end
+
+  pipeline :ensure_auth do
+    plug Guardian.Plug.EnsureAuthenticated
+  end
+
   pipeline :api do
     plug :accepts, ["json"]
   end
 
   scope "/", AuthAppWeb do
-    pipe_through :browser
+    pipe_through [:browser, :auth]
 
     get "/", PageController, :index
+    post "/authenticate", PageController, :authenticate
+  end
+
+  scope "/", AuthAppWeb do
+    pipe_through [:browser, :auth, :ensure_auth]
+
+    get "/dashboard", PageController, :dashboard
+    get "/logout", PageController, :logout
   end
 
   # Other scopes may use custom stacks.
