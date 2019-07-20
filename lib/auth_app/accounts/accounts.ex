ecdb26762b8045b75074d39fc47584e5b1e8e9cc
Ryan Angelo Cadag <ryan.cadag@secuna.io>
2019-07-20 22:59:24 +0800

    change get_user function and add function get_user_by_email/1

--- lib/auth_app/accounts/accounts.ex
+++ lib/auth_app/accounts/accounts.ex
@@ -5,15 +5,16 @@ defmodule AuthApp.Accounts do
 
   @spec create_user(map()) :: {:ok, %User{}}
   def create_user(attrs) do
-    User
+    %User{}
     |> User.changeset(attrs)
     |> Repo.insert()
   end
 
-  @spec get_user(atom(), String.id()) :: %User{}
-  def get_user(key, value) do
-    Repo.get_by(User, key, value)
-  end
+  @spec get_user(String.id()) :: %User{}
+  def get_user(id), do: Repo.get(User, id)
+
+  @spec get_user_by_email(String.t()) :: %User{}
+  def get_user_by_email(email), do: Repo.get_by(User, [email: email])
 
   @spec update_user(%User{}, map()) :: {:ok, %User{}}
   def update_user(user, attrs) do
